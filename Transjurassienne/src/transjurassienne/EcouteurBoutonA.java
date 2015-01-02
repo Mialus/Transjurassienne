02832d86fb53015d961f1d8882079486aa5b1469
Erizino <temporos.biohazard@hotmail.fr>
2015-01-02 20:36:09 +0100

    Update

--- Transjurassienne/src/transjurassienne/EcouteurBoutonA.java
+++ Transjurassienne/src/transjurassienne/EcouteurBoutonA.java
@@ -5,6 +5,8 @@
  */
 package transjurassienne;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
 
 /**
@@ -13,13 +15,21 @@ import javax.swing.*;
  */
 public class EcouteurBoutonA implements ActionListener {
     	private Fenetre ff;
+        private Data d;
         
-    	public EcouteurBoutonA(Fenetre f,Data d) {
+    	public EcouteurBoutonA(Fenetre f,Data di) {
             		ff=f;
+                        d=di;
         }
         
         	public void actionPerformed(ActionEvent e) {
                 String retour = JOptionPane.showInputDialog(ff, "veuillez donner le nom du coureur :", "le titre", 1);
-
+                // TODO
+                List<Categorie> liste = d.getListeClassement(retour);
+                if (liste==null){ 
+			System.out.println("aucun choix fait");
+                }else{
+                    PersonneF pf = new PersonneF(d);
+                }
 	}
 }
